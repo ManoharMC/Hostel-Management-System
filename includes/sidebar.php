dae14737d26fec16dd3dc3fb2e120c5468ae9b0c
ManoharMC <63175242+ManoharMC@users.noreply.github.com>
2020-04-05 07:41:56 +0530

    hostel management sytsem

---
+++ includes/sidebar.php
@@ -0,0 +1,53 @@
+						<nav class="ts-sidebar">
+			<ul class="ts-sidebar-menu">
+			
+				<li class="ts-label">Main</li>
+				
+
+					<li><a href="#"><i class="fa fa-files-o"></i> Master</a>
+					<ul>
+						<li><a href="roomtype.php"> Add RoomType</a></li>
+						<li><a href="room.php">Add Room</a></li>
+						<li><a href="department.php">department</a></li>					
+						<li><a href="student.php"> Add Student</a></li>
+				
+					</ul>
+				</li>
+					<li><a href="#"><i class="fa fa-desktop"></i> Link</a>
+					<ul>
+						
+						
+						<li><a href="room map.php"> Room Map</a></li>	
+						
+					</ul>
+				</li>
+				<li><a href="#"><i class="fa fa-desktop"></i> mess master</a>
+					<ul>
+						<li><a href="messbill master.php">messbill master</a></li>
+						
+						
+						<li><a href="modifyattendence.php">modify attendence</a></li>
+						
+												
+
+					</ul>
+				</li>
+				
+									<li><a href="#"><i class="fa fa-desktop"></i> Reports</a>
+					<ul>
+						
+						
+						<li><a href="roomwiserep.php"> Room Reports</a></li>	
+						<li><a href="studentwiserep.php"> Student Reports</a></li>	
+						
+					</ul>
+				</li>
+				
+
+				
+				
+				
+				
+
+			
+		</nav>
